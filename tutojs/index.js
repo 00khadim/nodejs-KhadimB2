3760d2146fc9f67f8dea8acfa01fda77bb5a251f
unknown <Ecole@cab-kcisse.TRHEA.LOCAL>
2022-12-20 22:56:28 +0100

    soir du 20/12/2022

--- tutojs/index.js
+++ tutojs/index.js
@@ -1,8 +1,9 @@
-const fs = require('fs');             // fonction
 
-fs.readdir('.', (erreur, files) => {   // Appelle la fonction readdir passe en parametre erreur et la liste des ficher
-    if(erreur){                        //Message d'erreur
-        console.log(erreur);
-    }
-  console.log(files);                 // Le ficher dans le repertoire courant
-});
+const fs = require('fs');                               
+
+let name_File = "myFile.txt"                                //  Nom du ficher 
+let contenu = "Mon contenue est bien present!"              //  Contenu du ficher
+fs.appendFile(name_File, contenu, function (err) {          // Fonction pour ajouter un ficher dans le repertoire dans la quelle on se trouve
+    if (err) throw err;
+    console.log("Fichier créé et il s'appelle", name_File); //Afficher le nom du ficher
+}); 
