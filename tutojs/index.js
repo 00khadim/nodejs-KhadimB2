7c4cc5d23700b19e56a627d07e7e2f283b8557ac
unknown <Ecole@cab-kcisse.TRHEA.LOCAL>
2022-12-21 10:51:42 +0100

    TP2 jour 3

--- tutojs/index.js
+++ tutojs/index.js
@@ -1,5 +1,27 @@
-const os = require('os');
+// importer lesmodules fs et http (regarde la doc)
+const fs = require("fs");
+const http = require('http');
 
-const repertoire = os.homedir();           //repertoire personelle de l'user
 
-console.log("Le répertoire personnel de l'utilisateur est :", repertoire);
+//Creation du server 
+http.createServer((httpRequest, httpReponse) => {
+
+  console.log('request; ', httpRequest)
+
+  fs.readFile("myFile.txt", "utf-8", (erreur, data) => {        //Où chercher le contenu à afficher
+    if (erreur) {
+      console.log(erreur);
+      //return;
+ 
+      // Erreur à afficher, avec le code 500 : reponse erreur serveur (Codes d'état de réponse HTTP)
+      httpReponse.statusCode = 500;
+      httpReponse.end('An error has occured.');
+      return
+    }
+    httpReponse.setHeader('content-Type', 'text/plain');      //Type de contenu du ficher 
+    httpReponse.end(data);                                      //Data = le contenu du ficher MyFile.txt
+
+  })
+}).listen(3000,() => {                                          //Port localhost dans la quelle sera present le l'erreur ou le contenu du ficher
+    console.log('Server web listing on port 3000');
+});
